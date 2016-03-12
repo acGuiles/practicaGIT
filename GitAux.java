090f78aa72dcacf58b29cb3c3b895844989ada49
Cristobal <cristobaljr95@uma.es>
2016-03-12 12:12:04 +0100

    mensaje descriptivo

--- GitAux.java
+++ GitAux.java
@@ -6,10 +6,10 @@ package prgitejemplo;
 
 /**
  *
- * @author bertoa
+
  */
 public class GitAux {
-    
+    //esto esta vacio
     public void metodoAlumno1(){
         System.out.println("metodo 1");
     }
@@ -19,6 +19,6 @@ public class GitAux {
     }
     
     public void metodoComunitario(){
-        System.out.println("Aqui escribimos todos");
+        System.out.println("Aqui no escribimos ninguno");
     }
 }
